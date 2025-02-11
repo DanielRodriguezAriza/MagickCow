aa58947ac377562b250863349660ac8efeef7878
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-11 22:39:32 +0100

    Created MagickCowMesh class

--- magickcow.py
+++ magickcow.py
@@ -68,6 +68,45 @@ class GSDObject:
 
 # endregion
 
+# region Meshes
+
+class MagickCowMesh:
+    def __init__(self, obj, transform):
+        self.obj = obj
+        self.transform = transform
+        self.invtrans = transform.inverted().transposed()
+        self.mesh = None
+        self.bm = None
+        self._calculate_mesh_data() # Apply modifiers, triangulate mesh, generate bm, etc...
+    
+    def __del__(self):
+        self.bm.free()
+
+    def _calculate_mesh_data(self):
+        self._select_object(self.obj)
+        self._apply_modifiers(self.obj)
+        self._triangulate_mesh(self.obj)
+    
+    def _select_object(self, obj):
+        obj.select_set(state = True)
+        bpy.context.view_layer.objects.active = obj
+
+    def _apply_modifiers(self, obj):
+        for mod in obj.modifiers:
+            bpy.ops.object.modifier_apply(modifier = mod.name)
+    
+    def _triangulate_mesh(self, obj):
+        mesh = obj.data
+        bm = bmesh.new()
+        bm.from_mesh(mesh)
+        bmesh.ops.triangulate(bm, faces=bm.faces)
+        bm.to_mesh(mesh)
+        self.mesh = mesh
+        self.bm = bm
+
+
+# endregion
+
 # region XNA classes
 
 # NOTE : This class' implementation looks to me like it's pretty inefficient and could be improved by a lot by using a single linear buffer rather than a list of lists, but whatever... we'll deal with this shit for now.
@@ -1221,8 +1260,9 @@ class DataGenerator:
     # endregion
 
     def generate_mesh_data(self, obj, transform, material_index = 0):
-        
         invtrans = transform.inverted().transposed()
+
+
 
     # endregion
 
