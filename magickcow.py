fb98c11ccd57f731079a99cd15e811ce618a78b0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-12 04:56:30 +0100

    Added explanatory comments
    The comment is kind of sad and useless, but anyway...
    
    The point is that I just installed the python vs code extensions considering how much the project has grown, maybe this will make things a little bit easier to deal with in the long run...

--- magickcow.py
+++ magickcow.py
@@ -27,6 +27,10 @@ bl_info = {
 
 # region Imports
 
+# NOTE : 12/02/2025 @ 4:53 AM Just installed the fake bpy module and python module for VS Code for the first time... bruh...
+# This pylint line is not really needed, but some older versions of VS Code appear to have some issues loading the fake bpy module, so I'm adding it here for anyone who uses
+# VS Code as their text editor and decides to check out the inner workings of this addon's code...
+# pylint: disable=fixme, import-error
 import bpy
 import bpy_extras
 import json
@@ -37,7 +41,7 @@ import array
 import math
 import mathutils
 import time
-from collections import namedtuple
+from collections import namedtuple # TODO : Get rid of this fucker, namedtuples are way slower than regular tuples AND run of the mill classes, so this module was pretty much a waste of time...
 
 # endregion
 
