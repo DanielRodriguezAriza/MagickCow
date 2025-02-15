08ff02fdb2728fd861c246f8d59ea9a8d5fb0f15
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-15 20:50:54 +0100

    Modified exception scope in script generator to encapsulate all file I/O operations

--- src/generator/generate.py
+++ src/generator/generate.py
@@ -16,12 +16,12 @@ def mcow_file_append(write_file, filename):
         write_file.write("\n") # I would write \r\n in Windows, but doing so leads to \r\r\n since \n is translated to \r\n automatically when working with "r" and "w" modes rather than "rb" and "wb". In short, python handles text mode operations for us already so we don't have anything to worry about.
 
 def mcow_file_generate(out_filename, in_filenames):
-    with open(out_filename, "w") as file:
-        try:
+    try:
+        with open(out_filename, "w") as file:
             for filename in in_filenames:
                 mcow_file_append(file, filename)
-        except Exception as e:
-            print(f"There was an error generating the output file: {e}")
+    except Exception as e:
+        print(f"There was an error generating the output file: {e}")
 
 def main():
     ofilename = "../../magickcow.py"
