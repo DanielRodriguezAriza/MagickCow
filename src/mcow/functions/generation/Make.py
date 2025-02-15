ac96652f9cd81da634fae9c61486a9d33de48684
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-15 21:58:28 +0100

    Added new classes for Make stage data generation
    TODO : Implement internal logic

--- src/mcow/functions/generation/Make.py
+++ src/mcow/functions/generation/Make.py
@@ -1,5 +1,19 @@
 # region Make Stage
 
+# This section contains classes whose purpose is to define the logic of the Make Stage of the code.
+
 # TODO : Move logic from data generation classes into external functions and place them here...
+
+# Base Data Maker class.
+class MCow_Data_Maker:
+    pass
+
+# Data Maker class for Maps / Levels
+class MCow_Data_Maker_Map(MCow_Data_Maker):
+    pass
+
+# Data Maker class for Physics entities
+class MCow_Data_Maker_PhysicsEntity(MCow_Data_Maker):
+    pass
 
 # endregion
