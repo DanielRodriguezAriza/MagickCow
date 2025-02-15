ccc62df7a72795209f31e873a7e605109b4ccf96
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-15 20:55:50 +0100

    Created new debug logging function and modified code to log through mcow_debug_log()

--- src/generator/generate.py
+++ src/generator/generate.py
@@ -9,22 +9,25 @@
 
 # endregion
 
+def mcow_debug_log(message):
+    print(f"[Generator] : {message}")
+
 def mcow_file_append(write_file, filename):
-    print(f"Appending File : {filename}")
+    mcow_debug_log(f"Appending File : {filename}")
     with open(filename, "r") as read_file:
         contents = read_file.read()
         write_file.write(contents)
         write_file.write("\n") # I would write \r\n in Windows, but doing so leads to \r\r\n since \n is translated to \r\n automatically when working with "r" and "w" modes rather than "rb" and "wb". In short, python handles text mode operations for us already so we don't have anything to worry about.
 
 def mcow_file_generate(out_filename, in_filenames):
-    print(f"Generating File : {out_filename}")
+    mcow_debug_log(f"Generating File : {out_filename}")
     try:
         with open(out_filename, "w") as file:
             for filename in in_filenames:
                 mcow_file_append(file, filename)
-        print("Data successfully generated!")
+        mcow_debug_log("Data successfully generated!")
     except Exception as e:
-        print(f"There was an error generating the output file: {e}")
+        mcow_debug_log(f"There was an error generating the output file: {e}")
 
 def main():
     ofilename = "../../magickcow.py"
