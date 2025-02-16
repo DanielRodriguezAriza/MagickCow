c557b04f098f3c4c92ed057a2eba20c4be48d036
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-16 05:00:43 +0100

    Added placeholder code for Make stage classes

--- src/mcow/functions/generation/Make.py
+++ src/mcow/functions/generation/Make.py
@@ -6,14 +6,33 @@
 
 # Base Data Maker class.
 class MCow_Data_Maker:
-    pass
+    def __init__(self):
+        return
+    
+    def make(self):
+        ans = {} # We return an empty object by default since this is the base class and it doesn't really implement any type of object data generation anyway, so yeah.
+        return ans
 
 # Data Maker class for Maps / Levels
 class MCow_Data_Maker_Map(MCow_Data_Maker):
-    pass
+    def __init__(self):
+        super().__init__()
+        return
+    
+    def make(self, make_data):
+        # TODO : Implement
+        ans = {}
+        return ans
 
 # Data Maker class for Physics entities
 class MCow_Data_Maker_PhysicsEntity(MCow_Data_Maker):
-    pass
+    def __init__(self):
+        super().__init__()
+        return
+    
+    def make(self, make_data):
+        # TODO : Implement
+        ans = {}
+        return ans
 
 # endregion
