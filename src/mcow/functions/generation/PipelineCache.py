66e922326e42e9de26f236ffdd1945abde8c1a78
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-16 15:34:36 +0100

    Added pipeline cache class
    The purpose of this class is to cache data that is shared across steps of the data pipeline in MagickCow.

---
+++ src/mcow/functions/generation/PipelineCache.py
@@ -0,0 +1,9 @@
+# region Pipeline Cache Classes
+
+# The classes within this region correspond to cache dicts and other cache objects that must be shared across different steps of the pipeline.
+
+# TODO : Implement
+class MCow_Data_Pipeline_Cache:
+    pass
+
+# endregion
