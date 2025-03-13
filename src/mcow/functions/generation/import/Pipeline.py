db7235ca6fb62f786624a64d23ff2217143bccd5
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-13 17:26:13 +0100

    Created simple layout for classes for the import pipeline

---
+++ src/mcow/functions/generation/import/Pipeline.py
@@ -0,0 +1,12 @@
+# region Import Data Generation pipeline classes
+
+class MCow_ImportPipeline:
+    pass
+
+class MCow_ImportPipeline_Map(ImportPipeline):
+    pass
+
+class MCow_ImportPipeline_PhysicsEntity(ImportPipeline):
+    pass
+
+# endregion
