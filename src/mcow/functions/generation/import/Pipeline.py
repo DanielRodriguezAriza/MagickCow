25dbf76664df34af9af1a83ab0572b490dcb8499
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-14 02:36:35 +0100

    Added basic exception handling to import pipeline exec methods

--- src/mcow/functions/generation/import/Pipeline.py
+++ src/mcow/functions/generation/import/Pipeline.py
@@ -5,20 +5,20 @@ class MCow_ImportPipeline:
         return
     
     def exec(self, data):
-        return
+        return MagickCowImportException() # The base class should never be used because it does not implement any specific type of pipeline for any kind of object, so it literally cannot import any sort of data...
 
 class MCow_ImportPipeline_Map(MCow_ImportPipeline):
     def __init__(self):
         return
     
     def exec(self, data):
-        return
+        raise MagickCowNotImplementedException()
 
 class MCow_ImportPipeline_PhysicsEntity(MCow_ImportPipeline):
     def __init__(self):
         return
     
     def exec(self, data):
-        return
+        raise MagickCowNotImplementedException()
 
 # endregion
