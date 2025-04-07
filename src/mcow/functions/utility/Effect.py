edc92c340287e0f462e00dec163198682c245890
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-07 15:25:58 +0200

    Added namespacing to material utility functions

--- src/mcow/functions/utility/Effect.py
+++ src/mcow/functions/utility/Effect.py
@@ -2,33 +2,42 @@
 
 # NOTE : Magicka / XNA "Effects" are linked to Blender Materials on this Blender addon.
 
-def utility_get_effect_name(mesh, material_index):
-    material_name = mesh.materials[material_index].name if len(mesh.materials) > 0 else utility_get_effect_name_default(mesh.magickcow_mesh_type)
-    return material_name
+# NOTE : Here I'm sort of experimenting with static classes with @staticmethod to simulate namespaces in python... we'll see how it goes...
+# Also going back to my snake_case C roots, which is more in line with PEP8. I'm just kind of tired of C#'s / Microsoft PascalCase I suppose.
 
-def utility_get_effect_name_default(fallback_type = "GEOMETRY"):
-    ans = "base/default"
+def class material_utility:
+    
+    # NOTE : The mesh param here is the same as obj, NOT obj.data!!!
+    
+    @staticmethod
+    def get_material_name(mesh, material_index):
+        material_name = mesh.materials[material_index].name if len(mesh.materials) > 0 else MaterialUtility.get_material_name_default(mesh.magickcow_mesh_type)
+        return material_name
 
-    if fallback_type == "GEOMETRY":
-        ans = "base/default_geometry"
-    elif fallback_type == "WATER":
-        ans = "base/default_water"
-    elif fallback_type == "LAVA":
-        ans = "base/default_lava"
-    elif fallback_type == "FORCE_FIELD":
-        ans = "base/ff/default_force_field"
+    @staticmethod
+    def get_material_name_default(fallback_type = "GEOMETRY"):
+        ans = "base/default"
 
-    return ans
+        if fallback_type == "GEOMETRY":
+            ans = "base/default_geometry"
+        elif fallback_type == "WATER":
+            ans = "base/default_water"
+        elif fallback_type == "LAVA":
+            ans = "base/default_lava"
+        elif fallback_type == "FORCE_FIELD":
+            ans = "base/ff/default_force_field"
 
-# TODO : Implement
-def utility_get_effect_data(material):
-    ans = {}
-    if material.mcow_effect_mode == "DOC":
-        # Get material data from JSON
-        ans = get_json_object(material.)
-    else:
-        # Get material data from material panel
-        pass
-    return ans
+        return ans
+
+    @staticmethod
+    def get_material_data(material):
+        ans = {}
+        if material.mcow_effect_mode == "DOC":
+            # Get material data from JSON
+            ans = get_json_object(material.)
+        else:
+            # Get material data from material panel
+            pass
+        return ans
 
 # endregion
