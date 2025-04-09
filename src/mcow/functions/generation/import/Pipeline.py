5aea26d08fafd6b7979618fc2fa079a61d9679d1
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-09 22:24:55 +0200

    Added function signatures for import methods for level model (map) import

--- src/mcow/functions/generation/import/Pipeline.py
+++ src/mcow/functions/generation/import/Pipeline.py
@@ -9,6 +9,7 @@ class MCow_ImportPipeline:
         raise MagickCowImportException("Cannot execute base import pipeline!")
         return None
 
+# TODO : Implement all import functions...
 class MCow_ImportPipeline_Map(MCow_ImportPipeline):
     def __init__(self):
         super().__init__()
@@ -16,6 +17,45 @@ class MCow_ImportPipeline_Map(MCow_ImportPipeline):
     
     def exec(self, data):
         raise MagickCowNotImplementedException("Import Map is not implemented yet!")
+    
+    def import_leve_model(self, level_model):
+        pass
+
+    def import_model_mesh(self, model):
+        pass
+    
+    def import_animated_parts(self, animated_parts):
+        pass
+    
+    def import_lights(self, lights):
+        pass
+    
+    def import_effects(self, effects):
+        pass
+    
+    def import_physics_entities(self, physics_entities):
+        pass
+    
+    def import_liquids(self, liquids):
+        pass
+    
+    def import_force_fields(self, force_fields):
+        pass
+    
+    def import_model_collision(self, collision):
+        pass
+    
+    def import_camera_collision(self, collision):
+        pass
+    
+    def import_triggers(self, triggers):
+        pass
+    
+    def import_locators(self, locators):
+        pass
+    
+    def import_nav_mesh(self, nav_mesh):
+        pass
 
 class MCow_ImportPipeline_PhysicsEntity(MCow_ImportPipeline):
     def __init__(self):
