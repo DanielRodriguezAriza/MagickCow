8b6429a001c2252a8d25720da289668df7cea540
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-10 13:53:24 +0200

    Added functions to read vector data in raw and read vector data with transform to Z up on import

--- src/mcow/functions/generation/import/Pipeline.py
+++ src/mcow/functions/generation/import/Pipeline.py
@@ -9,14 +9,32 @@ class MCow_ImportPipeline:
         raise MagickCowImportException("Cannot execute base import pipeline!")
         return None
     
-    def read_vector_3(self, vec3):
+    # region Read Methods - Raw
+
+    def read_vec2_raw(self, vec2):
+        ans = (vec2["x"], vec2["y"])
+        return ans
+
+    def read_vec3_raw(self, vec3):
         ans = (vec3["x"], vec3["y"], vec3["z"])
         return ans
     
-    def read_vector_4(self, vec4):
+    def read_vec4_raw(self, vec4):
         ans = (vec4["x"], vec4["y"], vec4["z"], vec4["w"])
         return ans
     
-    # TODO : Add option to handle reading vectors as Y up or Z up... maybe add a bool or a different method that specifies if we want to perform Y up to Z up conversion.
+    # endregion
+
+    # region Read Methods - Transform Y up to Z up
+    
+    def read_vec3_point(self, point):
+        ans = vec3_point_to_z_up(self.read_vec3_raw(point))
+        return
+    
+    def read_vec3_direction(self, direction):
+        # TODO : Implement
+        return None
+    
+    # endregion
 
 # endregion
