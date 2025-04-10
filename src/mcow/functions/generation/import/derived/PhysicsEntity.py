f8a8e0abf9bb111a55d90f71007d8ef8fc1cc55d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-10 03:01:45 +0200

    Created PhysicsEntity.py derived import pipeline class

---
+++ src/mcow/functions/generation/import/derived/PhysicsEntity.py
@@ -0,0 +1,11 @@
+# region Import Data Pipeline class - PhysicsEntity
+
+class MCow_ImportPipeline_PhysicsEntity(MCow_ImportPipeline):
+    def __init__(self):
+        super().__init__()
+        return
+    
+    def exec(self, data):
+        raise MagickCowNotImplementedException("Import PhysicsEntity is not implemented yet!")
+
+# endregion
