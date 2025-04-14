ce36ac468a8e86030cf6bb325df740ab5925476f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-14 04:10:22 +0200

    Fixed typo where VS decided to replace correct variable name with wrong variable name
    Fixed typo in code where VS decided that the singular variable name was not good enough, so it felt the need to autocomplete an extra fucking 's' at the end...

--- src/mcow/functions/generation/import/derived/Map.py
+++ src/mcow/functions/generation/import/derived/Map.py
@@ -291,19 +291,19 @@ class MCow_ImportPipeline_Map(MCow_ImportPipeline):
 
     def import_root_node(self, name, root_node):
         # Read root node properties
-        is_visible = root_nodes["isVisible"] # Ignored for now because we don't want meshes to be hidden on the scene on import. We'll maybe have an option that says "hide meshes that are set to invisible" or whatever on the mcow scene panel or some shit like that. Or maybe just import and hide the object and call it a day. Users can easily look for hidden objects and that's it.
-        casts_shadows = root_nodes["castsShadows"]
-        sway = root_nodes["sway"] # Not supported yet because we don't know wtf sway is yet...
-        entity_influence = root_nodes["entityInfluence"] # Not supported yet because we don't know wtf this does yet...
-        ground_level = root_nodes["groundLevel"] # Not supported yet because the exporter hardcodes this anyway, and we don't really know wtf this is yet so yeah...
-        vertex_stride = root_nodes["vertexStride"]
-        vertex_declaration = root_nodes["vertexDeclaration"]
-        vertex_buffer = root_nodes["vertexBuffer"]
-        index_buffer = root_nodes["indexBuffer"]
-        effect = root_nodes["effect"]
-        primitive_count = root_nodes["primitiveCount"]
-        start_index = root_nodes["startIndex"]
-        bounding_box = root_nodes["boundingBox"]
+        is_visible = root_node["isVisible"] # Ignored for now because we don't want meshes to be hidden on the scene on import. We'll maybe have an option that says "hide meshes that are set to invisible" or whatever on the mcow scene panel or some shit like that. Or maybe just import and hide the object and call it a day. Users can easily look for hidden objects and that's it.
+        casts_shadows = root_node["castsShadows"]
+        sway = root_node["sway"] # Not supported yet because we don't know wtf sway is yet...
+        entity_influence = root_node["entityInfluence"] # Not supported yet because we don't know wtf this does yet...
+        ground_level = root_node["groundLevel"] # Not supported yet because the exporter hardcodes this anyway, and we don't really know wtf this is yet so yeah...
+        vertex_stride = root_node["vertexStride"]
+        vertex_declaration = root_node["vertexDeclaration"]
+        vertex_buffer = root_node["vertexBuffer"]
+        index_buffer = root_node["indexBuffer"]
+        effect = root_node["effect"]
+        primitive_count = root_node["primitiveCount"]
+        start_index = root_node["startIndex"]
+        bounding_box = root_node["boundingBox"]
         
         # region Comment - Child nodes support
         
