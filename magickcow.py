a2a901644f66f3f887cb908c04813daa72044855
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-17 15:40:16 +0200

    Re-generated main mcow file

--- magickcow.py
+++ magickcow.py
@@ -953,10 +953,175 @@ def register_properties_material_geometry(material): # NOTE : Maybe this should 
         default = False
     )
 
+    material.mcow_effect_deferred_reflection_map_enabled = bpy.props.BoolProperty(
+        name = "Reflection Map Enabled",
+        default = False
+    )
+
+    material.mcow_effect_deferred_reflection_map = bpy.props.StringProperty(
+        name = "Reflection Map",
+        default = False
+    )
+
+    material.mcow_effect_deferred_diffuse_texture_0_alpha_disabled = bpy.props.BoolProperty(
+        name = "Diffuse Texture 0 Alpha Disabled",
+        default = True
+    )
+
+    material.mcow_effect_deferred_alpha_mask_0_enabled = bpy.props.BoolProperty(
+        name = "Alpha Mask 0 Enabled",
+        default = False
+    )
+
+    material.mcow_effect_deferred_diffuse_color_0 = bpy.props.bpy.props.FloatVectorProperty(
+        name = "Diffuse Color 0",
+        subtype = "COLOR",
+        default = (1.0, 1.0, 1.0),
+        min = 0.0,
+        max = 1.0,
+        size = 3
+    )
+
+    material.mcow_effect_deferred_specular_amount_0 = bpy.props.FloatProperty(
+        name = "Specular Amount 0",
+        default = 0
+    )
+
+    material.mcow_effect_deferred_specular_power_0 = bpy.props.FloatProperty(
+        name = "Specular Power 0",
+        default = 20
+    )
+
+    material.mcow_effect_deferred_emissive_amount_0 = bpy.props.FloatProperty(
+        name = "Emissive Amount 0",
+        default = 0
+    )
+
+    material.mcow_effect_deferred_normal_power_0 = bpy.props.FloatProperty(
+        name = "Normal Power 0",
+        default = 1
+    )
+
+    material.mcow_effect_deferred_reflection_intensity_0 = bpy.props.FloatProperty(
+        name = "Reflection Intensity 0",
+        default = 0
+    )
+
+    material.mcow_effect_deferred_diffuse_texture_0 = bpy.props.StringProperty(
+        name = "Diffuse Texture 0",
+        default = "..\\Textures\\Surface\\Nature\\Ground\\grass_lush00_0"
+    )
+
+    material.mcow_effect_deferred_material_texture_0 = bpy.props.StringProperty(
+        name = "Material Texture 0",
+        default = ""
+    )
+
+    material.mcow_effect_deferred_normal_texture_0 = bpy.props.StringProperty(
+        name = "Normal Texture 0",
+        default = ""
+    )
+
+    material.mcow_effect_deferred_has_second_set = bpy.props.BoolProperty(
+        name = "Has Second Set",
+        default = False
+    )
+
+    material.mcow_effect_deferred_diffuse_texture_1_alpha_disabled = bpy.props.BoolProperty(
+        name = "Diffuse Texture 1 Alpha Disabled",
+        default = True
+    )
+
+    material.mcow_effect_deferred_alpha_mask_1_enabled = bpy.props.BoolProperty(
+        name = "Alpha Mask 1 Enabled",
+        default = False
+    )
+
+    material.mcow_effect_deferred_diffuse_color_1 = bpy.props.bpy.props.FloatVectorProperty(
+        name = "Diffuse Color 1",
+        subtype = "COLOR",
+        default = (1.0, 1.0, 1.0),
+        min = 0.0,
+        max = 1.0,
+        size = 3
+    )
+
+    material.mcow_effect_deferred_specular_amount_1 = bpy.props.FloatProperty(
+        name = "Specular Amount 1",
+        default = 0.0
+    )
+
+    material.mcow_effect_deferred_specular_power_1 = bpy.props.FloatProperty(
+        name = "Specular Power 1",
+        default = 0.0
+    )
+
+    material.mcow_effect_deferred_emissive_amount_1 = bpy.props.FloatProperty(
+        name = "Emissive Amount 1",
+        default = 0.0
+    )
+
+    material.mcow_effect_deferred_normal_power_1 = bpy.props.FloatProperty(
+        name = "Normal Power 1",
+        default = 0.0
+    )
+
+    material.mcow_effect_deferred_reflection_intensity_1 = bpy.props.FloatProperty(
+        name = "Reflection Intensity 1",
+        default = 0.0
+    )
+
+    material.mcow_effect_deferred_diffuse_texture_1 = bpy.props.StringProperty(
+        name = "Diffuse Texture 1",
+        default = ""
+    )
+
+    material.mcow_effect_deferred_material_texture_1 = bpy.props.StringProperty(
+        name = "Material Texture 1",
+        default = ""
+    )
+
+    material.mcow_effect_deferred_normal_texture_1 = bpy.props.StringProperty(
+        name = "Normal Texture 1",
+        default = ""
+    )
+
+    # NOTE : On the GUI side, maybe expose these properties under the labels "texture set 0" and "texture set 1" or whatever the fuck so that we can have some better organization,
+    # maybe even make the window expandable or whatever, or add an enabled / disabled option for the second texture / material info set. Something like "bool secondMaterialSetEnabled"...
+
 def unregister_properties_material_geometry(material):
     del material.mcow_effect_deferred_alpha
     del material.mcow_effect_deferred_sharpness
     del material.mcow_effect_deferred_vertex_color_enabled
+
+    del material.mcow_effect_deferred_reflection_map_enabled
+    del material.mcow_effect_deferred_reflection_map
+
+    del material.mcow_effect_deferred_diffuse_texture_0_alpha_disabled
+    del material.mcow_effect_deferred_alpha_mask_0_enabled
+    del material.mcow_effect_deferred_diffuse_color_0
+    del material.mcow_effect_deferred_specular_amount_0
+    del material.mcow_effect_deferred_specular_power_0
+    del material.mcow_effect_deferred_emissive_amount_0
+    del material.mcow_effect_deferred_normal_power_0
+    del material.mcow_effect_deferred_reflection_intensity_0
+    del material.mcow_effect_deferred_diffuse_texture_0
+    del material.mcow_effect_deferred_material_texture_0
+    del material.mcow_effect_deferred_normal_texture_0
+
+    del material.mcow_effect_deferred_has_second_set
+
+    del material.mcow_effect_deferred_diffuse_texture_1_alpha_disabled
+    del material.mcow_effect_deferred_alpha_mask_1_enabled
+    del material.mcow_effect_deferred_diffuse_color_1
+    del material.mcow_effect_deferred_specular_amount_1
+    del material.mcow_effect_deferred_specular_power_1
+    del material.mcow_effect_deferred_emissive_amount_1
+    del material.mcow_effect_deferred_normal_power_1
+    del material.mcow_effect_deferred_reflection_intensity_1
+    del material.mcow_effect_deferred_diffuse_texture_1
+    del material.mcow_effect_deferred_material_texture_1
+    del material.mcow_effect_deferred_normal_texture_1
 
 def register_properties_material_water(material):
     pass
