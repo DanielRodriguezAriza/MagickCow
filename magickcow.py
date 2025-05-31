6ff7bc1a8f3fbdb1731cd3e3d9c6d48e896a8c9b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-31 20:00:41 +0200

    Re-generated main mcow file

--- magickcow.py
+++ magickcow.py
@@ -105,6 +105,32 @@ import pathlib
 
 # endregion
 
+# ../mcow/globals/Globals.py
+# region Globals
+
+# NOTE : This file contains all of the global constants that mcow relies on.
+# It's a latter addition to the mcow implementation, so plenty of constants are still hardcoded in-place and need to be modified to use this instead.
+
+# List of all supported image format file extensions.
+# This list contains all of the officially supported image file formats found within Blender's documentation (https://docs.blender.org/manual/en/latest/files/media/image_formats.html)
+# Last updated 31/05/2025@19:58
+MCOW_IMAGE_EXTENSIONS = [
+    "bmp",
+    "sgi", "rgb", "bw",
+    "png",
+    "jpg", "jpeg",
+    "jp2", "j2c",
+    "tga",
+    "cin",
+    "dpx",
+    "exr",
+    "hdr",
+    "tif", "tiff",
+    "webp"
+]
+
+# endregion
+
 # ../mcow/classes/MagickCow/Exception.py
 # region Exception Classes
 
