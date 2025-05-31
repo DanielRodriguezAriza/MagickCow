305b40d2e1fbc94fd688370627c3b1bfdc8735d7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-31 19:55:06 +0200

    Created source file to contain global constants

---
+++ src/mcow/globals/Globals.py
@@ -0,0 +1,6 @@
+# region Globals
+
+# NOTE : This file contains all of the global constants that mcow relies on.
+# It's a latter addition to the mcow implementation, so plenty of constants are still hardcoded in-place and need to be modified to use this instead.
+
+# endregion
