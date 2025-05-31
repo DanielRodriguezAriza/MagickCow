1e5fa74dff2b2795620c81928493609bd66f7bd2
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-31 17:00:46 +0200

    Added texture utility functions file
    TODO : Implement

---
+++ src/mcow/functions/utility/Texture.py
@@ -0,0 +1,6 @@
+# region Utility Functions for handling Texture data
+
+# NOTE : In the future, maybe move the texture loading utility functions here, once they become free functions and we pass around the CTX var instead of "self."
+# Either that or get rid of this file.
+
+# endregion
