c0833dd45fe308fd65951db8285f8ce24748dc4a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-07-07 15:24:58 +0200

    Added missing import to shutil

--- MagickCow/src/Imports.py
+++ MagickCow/src/Imports.py
@@ -17,5 +17,6 @@ import time
 from collections import namedtuple # TODO : Get rid of this fucker, namedtuples are way slower than regular tuples AND run of the mill classes, so this module was pretty much a waste of time...
 
 import pathlib
+import shutil
 
 # endregion
