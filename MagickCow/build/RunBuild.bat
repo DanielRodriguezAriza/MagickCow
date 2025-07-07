9631d98660a3ae3ef8bdc467869fa45267db103a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-07-07 13:18:00 +0200

    Added build scripts for windows and linux

---
+++ MagickCow/build/RunBuild.bat
@@ -0,0 +1,4 @@
+@echo off
+python3 generate.py
+echo Press ^<ENTER^> to terminate process
+pause > NUL
---
+++ MagickCow/build/RunBuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 generate.py
+echo "Press <ENTER> to terminate process"
+read
