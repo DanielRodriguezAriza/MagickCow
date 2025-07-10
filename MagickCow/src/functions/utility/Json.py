8ee40996efca85ef79308bb08f0fe97a3263c3e3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-07-10 21:13:48 +0200

    Added utility file with json related functions

---
+++ MagickCow/src/functions/utility/Json.py
@@ -0,0 +1,14 @@
+# region Json Utility
+
+# TODO : Either implement these or just discard them.
+# json.dumps and json.loads is nice enough, but offering my own thing could be good to. Something to think about in the future...
+
+mcow_json_loads(in_dict):
+    # TODO : Implement
+    return {}
+
+mcow_json_dumps(in_str):
+    # TODO : Implement
+    return ""
+
+# endregion
