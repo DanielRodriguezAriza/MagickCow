b9e78a916a99f190a93b3e906f5cd72b2a4b3b93
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-08-09 00:50:01 +0200

    Implemented aux utility function to append group nodes with override

--- MagickCow/src/functions/utility/Blend.py
+++ MagickCow/src/functions/utility/Blend.py
@@ -16,4 +16,26 @@ def mcow_utility_append_blender_group_nodes(path, link = False):
     with bpy.data.libraries.load(path, link = link) as (data_from, data_to):
         data_to.node_groups = data_from.node_groups
 
+# Append the material group nodes stored within the specified blender file to the current one with override (those whose name are shared will be overwritten with the new data)
+def mcow_utility_append_blender_group_nodes(path, link = False):
+    
+    with bpy.data.libraries.load(path, link = link) as (data_from, data_to):
+
+        node_groups_names_pairs = []
+
+        for new_node_group in data_from.node_groups:
+            old_node_group = bpy.data.node_groups.get(new_node_group.name)
+            if node is not None:
+                old_node_group.name = old_node_group.name + "_OLD"
+                node_groups_names_pairs.append((old_node_group.name, new_node_group.name))
+
+        data_to.node_groups = data_from.node_groups
+
+        for node_group_name_pair in node_groups_names_pairs:
+            old_name, new_name = node_group_name_pair
+            old_node = bpy.data.node_groups[old_name]
+            new_node = bpy.data.node_groups[new_name]
+            old_node.user_remap(new_node)
+            bpy.data.node_groups.remove(old_node)
+
 # endregion
